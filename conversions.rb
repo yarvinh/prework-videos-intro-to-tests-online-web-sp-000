59e7f4e84cbbded9ef48c1d4699c6809128a0891
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-06 04:23:06 +0000

    Automatically backed up by Learn

--- conversions.rb
+++ conversions.rb
@@ -1,4 +1,4 @@
-GRAMS_PER_OUNCE = 28.3495
-def ounces_to_grams(ounces)
-  ounces.to_f * GRAMS_PER_OUNCE
+POUNDS_PER_KILOS = 28.3495
+def ounces_to_grams(kilos)
+  ounces.to_f * POUNDS_PER_KILOS
 end
